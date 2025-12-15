5ebabb3e7f0f3559ad3682a0a7609639eb190641
PRIME-07 <anujm0707@gmail.com>
2025-12-15 11:16:53 +0530

    minor fixes

--- model/embedding_block.py
+++ model/embedding_block.py
@@ -1,22 +1,22 @@
-    import torch.nn as nn
-    from config import Config
-    from token_embeddings import TokenEmbedding
-    from positional_embeddings import PositionalEmbedding
+import torch.nn as nn
+from config import Config
+from token_embeddings import TokenEmbedding
+from positional_embeddings import PositionalEmbedding
 
 
-    class EmbeddingBlock(nn.Module):
-        def __init__(self, config):
-            super().__init__()
-            self.token_embedding = TokenEmbedding(config)
-            self.positional_embedding = PositionalEmbedding(config)
-            self.dropout = nn.Dropout(config.dropout)
+class EmbeddingBlock(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.token_embedding = TokenEmbedding(config)
+        self.positional_embedding = PositionalEmbedding(config)
+        self.dropout = nn.Dropout(config.dropout)
 
-        def forward(self, input_ids):
-            # Get the token embeddings
-            token_emb = self.token_embedding(input_ids)
-            # Get the positional embeddings
-            pos_emb = self.positional_embedding(input_ids)
-            # Add the token and positional embeddings and apply dropout
-            combined_emb = self.dropout(token_emb + pos_emb)
-            
-            return combined_emb
+    def forward(self, input_ids):
+        # Get the token embeddings
+        token_emb = self.token_embedding(input_ids)
+        # Get the positional embeddings
+        pos_emb = self.positional_embedding(input_ids)
+        # Add the token and positional embeddings and apply dropout
+        combined_emb = self.dropout(token_emb + pos_emb)
+        
+        return combined_emb
